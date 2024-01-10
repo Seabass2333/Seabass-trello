368f45ef7716334725999524a04f1c366e930a2f
luhan <luhan@360.cn>
2024-01-10 15:00:12 +0800

    feat: home + nav + footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,29 @@
+import Link from 'next/link'
+import localFont from 'next/font/local'
+import { Poppins } from 'next/font/google'
 import { Medal } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+
+const headingFont = localFont({
+  src: '../../public/fonts/font.woff2'
+})
+
+const textFont = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
 const Marketing = () => {
   return (
     <div className='flex items-center justify-center flex-col'>
-      <div className='flex items-center justify-center flex-col'>
+      <div
+        className={cn(
+          'flex items-center justify-center flex-col',
+          headingFont.className
+        )}
+      >
         <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase'>
           <Medal className='h-6 w-6 mr-2'></Medal>
           No1 task management
@@ -15,11 +35,19 @@ const Marketing = () => {
           work forward
         </div>
       </div>
-      <div className='text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto'>
+      <div
+        className={cn(
+          'text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto',
+          textFont.className
+        )}
+      >
         Collaborate, manage projects, and do something different that can be
         improve my possible in the future competed, so do not be a rat, just do
         it!
       </div>
+      <Button className='mt-4' size='lg' asChild>
+        <Link href='/sign-up'>get Taskify for free</Link>
+      </Button>
     </div>
   )
 }
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: 'Taskify',
+  description: 'Collaborate, manage projects, and do something different that can be improve my possible in the future competed, so do not be a rat, just do it!'
+}
